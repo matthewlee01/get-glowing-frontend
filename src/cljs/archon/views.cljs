03bfa963f1fc36bf1ba0c975bdba15643d4436ae
Jay <jaylee69@gmail.com>
2019-03-18 21:40:21 -0700

    added some very basic skeleton views for the landing page and the vendors page

--- src/cljs/archon/views.cljs
+++ src/cljs/archon/views.cljs
@@ -4,8 +4,38 @@
    [archon.subs :as subs]
    ))
 
+(defn city-input []
+  [:div
+    [:label "City"]
+    [:input {:type "text"
+             :auto-focus true}]])
+
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "this is a re-frame project"]
-     ]))
+      [:h1 "glow"]
+      [:div
+        [:button "Become a vendor"]
+        [:button "Help"]
+        [:button "Login"]
+        [:button "Sign Up"]]
+      (city-input)]))
+      
+
+(defn service-input []
+  [:div
+    [:label "Service"]
+    [:input {:type "text"
+             :auto-focus true}]])
+
+(defn vendors-panel []
+  [:div
+    [:h1 "glow"]
+    [service-input]
+    [:div
+      [:ul
+         [:li "Vendor1"]
+         [:li "Vendor2"]
+         [:li "Vendor3"]]]])
+  
+
