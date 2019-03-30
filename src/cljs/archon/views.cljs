626f6cf166959edcc65c22824a1d67d429c01e56
Matthew Lee <laughew@gmail.com>
2019-03-30 12:42:30 -0700

    changed img src to reflect prev changes to file location

--- src/cljs/archon/views.cljs
+++ src/cljs/archon/views.cljs
@@ -21,7 +21,7 @@
 (defn vendor-card [vendor]
   (let [ {:keys [vendor_id name_first name_last addr_city profile_pic]} vendor]
     [:div
-     [:img {:src (str "http://localhost:8888/" profile_pic)
+     [:img {:src profile_pic
             :alt profile_pic}]
      [:p (str vendor_id " " name_first " " name_last " " addr_city)]]))
 
