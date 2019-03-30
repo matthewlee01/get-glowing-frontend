fe8a14f5e740264516c50b8f7657d014f7b65a5e
Matthew Lee <laughew@gmail.com>
2019-03-30 15:26:18 -0700

    added cljss to deps and added basic styling for title

--- src/cljs/archon/views.cljs
+++ src/cljs/archon/views.cljs
@@ -2,7 +2,10 @@
   (:require
    [re-frame.core :as re-frame]
    [archon.subs :as subs]
-   [archon.events :as events]))
+   [archon.events :as events]
+   [cljss.core :as css :refer-macros [defstyles defkeyframes]]
+   [cljss.reagent :refer-macros [defstyled]])
+  (:require-macros [cljss.core]))
    
 
 (defn city-input []
@@ -18,6 +21,7 @@
     [:label "Service"]
     [:input {:type "text"
              :auto-focus true}]])
+
 (defn vendor-card [vendor]
   (let [ {:keys [vendor_id name_first name_last addr_city profile_pic]} vendor]
     [:div
@@ -30,9 +34,19 @@
     (let [vendors @(re-frame/subscribe [::subs/vendor-list])]
       (map vendor-card vendors))])
 
+(defstyles title [font-size]
+  {:font-size (str font-size "px")
+   :color "#FFC0CB"
+   :margin "10px 10px"})
+
+(defstyled bigbutton :button
+  {:font-size "32px"
+   :background-color (with-meta #(get {:light "white" :dark "black"} %) :color)
+   :size "50px"})
+  
 (defn main-panel []
   [:div
-    [:h1 @(re-frame/subscribe [::subs/app-name])]
+    [:h1 {:class (title 80)} @(re-frame/subscribe [::subs/app-name])]
     [:div
       [:button "Become a vendor"]
       [:button "Help"]
