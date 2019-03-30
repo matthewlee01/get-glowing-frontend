ee3867e7549725f554e2c050df1ba1a93dbc57c1
Matthew Lee <laughew@gmail.com>
2019-03-30 15:51:04 -0700

    sexy buttons

--- src/cljs/archon/views.cljs
+++ src/cljs/archon/views.cljs
@@ -6,7 +6,31 @@
    [cljss.core :as css :refer-macros [defstyles defkeyframes]]
    [cljss.reagent :refer-macros [defstyled]])
   (:require-macros [cljss.core]))
-   
+ 
+(defstyles title [font-size]
+  {:font-size (str font-size "px")
+   :color "#FFC0CB"
+   :margin "10px 10px"})
+
+(defstyles list-pfp [size]
+  {:height (str size "px")
+   :width (str size "px")})
+
+(defstyled sexy-button :button
+  {:background-color "#7a7978"
+   :border "none"
+   :border-radius "4px"
+   :color "white"
+   :margin "10px"
+   :padding "15px 32px"
+   :text-align "center"
+   :text-decoration "none"
+   :display "inline-block"
+   :font-size "16px"
+   :opacity "0.6"
+   :cursor "pointer"
+   :transition "0.3s"
+   :&:hover {:opacity "1"}})
 
 (defn city-input []
   [:div
@@ -26,7 +50,8 @@
   (let [ {:keys [vendor_id name_first name_last addr_city profile_pic]} vendor]
     [:div
      [:img {:src profile_pic
-            :alt profile_pic}]
+            :alt profile_pic
+            :class (list-pfp 200)}]
      [:p (str vendor_id " " name_first " " name_last " " addr_city)]]))
 
 (defn vendors-panel []
@@ -34,24 +59,15 @@
     (let [vendors @(re-frame/subscribe [::subs/vendor-list])]
       (map vendor-card vendors))])
 
-(defstyles title [font-size]
-  {:font-size (str font-size "px")
-   :color "#FFC0CB"
-   :margin "10px 10px"})
-
-(defstyled bigbutton :button
-  {:font-size "32px"
-   :background-color (with-meta #(get {:light "white" :dark "black"} %) :color)
-   :size "50px"})
-  
 (defn main-panel []
   [:div
-    [:h1 {:class (title 80)} @(re-frame/subscribe [::subs/app-name])]
+    [:h1 {:class (title 80)
+          :on-click #(js/alert "hi")} @(re-frame/subscribe [::subs/app-name])]
     [:div
-      [:button "Become a vendor"]
-      [:button "Help"]
-      [:button "Login"]
-      [:button "Sign Up"]]
+      (sexy-button "Become a vendor")
+      (sexy-button "Help")
+      (sexy-button "Login")
+      (sexy-button "Sign Up")]
     (condp = @(re-frame/subscribe [::subs/active-panel])
       :vendors-panel [vendors-panel]
       :city-input [city-input])])
