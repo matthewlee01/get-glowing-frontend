4cfe817fdc699f6edaedc154f1ed5f01c1c1a3d3
jay <jay>
2019-04-03 04:09:16 +0000

    re-factor build and run scripts

--- bin/docker-buildandrun-nginx.sh
+++ bin/docker-buildandrun-nginx.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 lein cljsbuild once min &&
-docker build -t archon .. &&
-docker run -tid --rm --name archon_1 -p 80:80 archon
+docker build -t archon .. 
 
---
+++ bin/run-nginx.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker run -tid --rm --name archon_1 -p 80:80 archon
+
